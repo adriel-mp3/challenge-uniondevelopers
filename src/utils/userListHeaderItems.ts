5af8d5e1d02fc8cad578edb07e6000ea599a768e
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-22 21:11:51 -0300

    feat: add user list header items utils

---
+++ src/utils/userListHeaderItems.ts
@@ -0,0 +1,11 @@
+const headerItems = [
+  { label: "ID" },
+  { label: "First Name" },
+  { label: "Last Name" },
+  { label: "Title" },
+  { label: "Date" },
+  { label: "Age" },
+  { label: "Actions" },
+];
+
+export default headerItems;
