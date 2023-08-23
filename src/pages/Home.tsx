a2852d9cec09427ef8eeeb901317cb14d977886a
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-22 21:10:10 -0300

    feat: import UserList component to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import UserList from "../components/UserList";
+
 const Home = () => {
   return (
     <>
       <header className="home-header">
         <h1 className="heading-primary">List Users</h1>
         <div className="input-wrapper">
-          <input className="text-input" type="text" placeholder="Search user..."/>
+          <input
+            className="text-input"
+            type="text"
+            placeholder="Search user..."
+          />
         </div>
       </header>
+      <UserList />
     </>
   );
 };
