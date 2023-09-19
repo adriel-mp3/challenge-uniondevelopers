20206c6e3ea41521681012f8ee9c605cfd56bdc9
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-09-18 21:09:21 -0300

    :bug: fix hook types

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,44 +1,34 @@
 import React from "react";
 
-interface UseFetchResponse {
-  response: Response;
-  json: any;
-}
-
-interface ApiResponse<Data> {
-  data: Data;
-  loading: boolean;
-  error: string | null;
-  request: (url: string, options: object) => Promise<void>;
-}
-
-const useFetch = <Data = any>(): ApiResponse<Data> => {
+const useFetch = () => {
   const [data, setData] = React.useState(null);
   const [error, setError] = React.useState<string | null>(null);
   const [loading, setLoading] = React.useState(false);
 
-  const request = React.useCallback(async (url: string, options: object ) => {
-    let response: Response | null = null;
-    let json: any;
-
-    try {
-      setError(null);
-      setLoading(true);
-      response = await fetch(url, options);
-      json = await response.json();
-      if (response.ok === false) throw new Error(json.message);
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        json = null;
-        setError(err.message);
+  const request = React.useCallback(
+    async (url: RequestInfo | URL, options?: RequestInit) => {
+      let response;
+      let json;
+      fetch;
+      try {
+        setError(null);
+        setLoading(true);
+        response = await fetch(url, options);
+        json = await response.json();
+        if (response.ok === false) throw new Error(json.message);
+      } catch (err) {
+        if (err instanceof Error) {
+          json = null;
+          setError(err.message);
+        }
+      } finally {
+        setData(json);
+        setLoading(false);
       }
-    } finally {
-      setData(json);
-      setLoading(false);
-    }
-    const result: UseFetchResponse = { response: response as Response, json };
-    return result;
-  }, []);
+      return response;
+    },
+    []
+  );
   return { data, error, loading, request };
 };
 
